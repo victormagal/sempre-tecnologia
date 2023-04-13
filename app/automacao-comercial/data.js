4ca590dc5dd7212d13c594125af951c55a7d9856
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-04-13 16:51:37 -0300

    feat: pagina automacao comercial

---
+++ app/automacao-comercial/data.js
@@ -0,0 +1,24 @@
+const doubts = [
+  {
+    question: 'O sistema controle o recebimento dos cartões?',
+    answer:
+      'É possível ver os valores a receber por dia, por bandeira, por maquininha, além de acompanhar as taxas aplicadas em cada venda.'
+  },
+  {
+    question: 'Consigo transferir produtos de um estoque para outro?',
+    answer:
+      'Sim, temos a opção de trabalhar com um banco de dados web para centralizar as informações e assim permitir as movimentações entre os estoques das lojas.'
+  },
+  {
+    question: 'Posso criar grupos de mercadorias no cadastro de produto?',
+    answer:
+      'Além de conseguir fazer isso você pode separar por fornecedor, marca e modelo.'
+  },
+  {
+    question: 'Tem controle de clientes?',
+    answer:
+      'Você poderá manter o controle de clientes, fornecedores e produtos.'
+  }
+];
+
+export { doubts };
--- app/components/Elements/Card/index.js
+++ app/components/Elements/Card/index.js
@@ -6,10 +6,10 @@ export default function Card({ description, imageSource, title }) {
       <div className="bg-white drop-shadow-xl filter flex justify-center items-center h-20 rounded-full w-20">
         <Image
           alt="Sempre mensalidade"
-          height={37}
+          height={27}
           quality={100}
           src={imageSource}
-          width={39}
+          width={27}
         />
       </div>
       <h1 className="font-serif font-semibold my-6 text-dark-blue text-3xl">
