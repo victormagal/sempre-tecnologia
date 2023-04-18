4a104659a11db618f115cb6f2e7a38cda03f8ece
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-04-18 15:40:35 -0300

    feat: mdx content renderizando normalmente

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-undef */
 'use client';
-import { MDXRemote } from 'next-mdx-remote';
-import { serialize } from 'next-mdx-remote/serialize';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
@@ -10,24 +8,20 @@ import { getPost } from '../../graphql/queries';
 import { useQuery } from '@apollo/client';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MarkdownIt from 'markdown-it';
 
 export default function Post() {
-  const [content, setContent] = useState({});
+  const md = new MarkdownIt();
+  const [content, setContent] = useState();
   const [data, setData] = useState({});
   const path = usePathname().slice(6);
   useQuery(getPost, {
     variables: { slug: path },
-    onCompleted: async (data) => {
-      setContent(
-        await serialize(data?.blogPosts?.data[0]?.attributes?.content)
-      );
+    onCompleted: (data) => {
+      setContent(md.render(data?.blogPosts?.data[0]?.attributes?.content));
       setData(data?.blogPosts?.data[0]?.attributes);
     }
   });
-  // const serializeContent = serialize(
-  //   data?.blogPosts?.data[0]?.attributes?.content
-  // );
-  console.log(content);
 
   return (
     <main>
@@ -54,9 +48,10 @@ export default function Post() {
             src={`${process.env.NEXT_PUBLIC_UPLOADS_URL}${data?.image?.data?.attributes?.url}`}
           />
         </div>
-        <div className="col-span-12">
-          <MDXRemote {...content} />
-        </div>
+        <section
+          className="col-span-12"
+          dangerouslySetInnerHTML={{ __html: content }}
+        ></section>
       </Container>
     </main>
   );
