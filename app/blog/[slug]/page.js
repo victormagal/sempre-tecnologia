bd3f90d6a86c32f371d128de068c9ba1395a3fb4
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-04-18 23:37:47 -0300

    feat: pagina do post estilizada

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -2,7 +2,9 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import styles from './page.module.css';
 import { Container, HeroPage } from '../../components/Foundation';
 import { getPost } from '../../graphql/queries';
 import { useQuery } from '@apollo/client';
@@ -11,6 +13,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import MarkdownIt from 'markdown-it';
 
 export default function Post() {
+  const router = useRouter();
   const md = new MarkdownIt();
   const [content, setContent] = useState();
   const [data, setData] = useState({});
@@ -27,31 +30,44 @@ export default function Post() {
     <main>
       <HeroPage arrow={false} cta={false} mini={true} uri="/bg-blog.svg" />
       <Container newClasses="py-24">
-        <div className="col-span-2 flex items-center">
-          <FontAwesomeIcon
-            className="text-custom-orange"
-            icon={faChevronLeft}
-          />
-          <Link className="font-sans text-sm text-soft-gray" href="">
-            voltar para o blog
-          </Link>
-        </div>
-        <div className="col-span-10">
-          <p>Escrito por Sempre Tecnologia, {data?.updatedAt}</p>
-        </div>
-        <h1 className="col-span-12 font-serif font-bold text-center text-dark-blue text-5xl">
+        <header className="border-b col-span-12 grid grid-cols-12 gap-6 pb-4">
+          <div className="col-span-2 flex items-center">
+            <FontAwesomeIcon
+              className="text-custom-orange"
+              icon={faChevronLeft}
+            />
+            <Link
+              className="font-sans ml-4 text-sm text-soft-gray"
+              href=""
+              onClick={() => router.back()}
+            >
+              voltar para o blog
+            </Link>
+          </div>
+          <div className="col-span-10 flex justify-end">
+            <p className="font-sans text-sm text-soft-gray">
+              Escrito por Sempre Tecnologia,{' '}
+              {new Date(data?.updatedAt).toLocaleDateString('pt-BR', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+              })}
+            </p>
+          </div>
+        </header>
+        <h1 className="col-span-8 col-start-3 font-serif font-bold my-8 text-center text-dark-blue text-5xl">
           {data?.title}
         </h1>
-        <div className="col-span-12 flex justify-center">
+        <div className="col-span-8 col-start-3 flex justify-center mb-8">
           <img
             alt={data?.title}
             src={`${process.env.NEXT_PUBLIC_UPLOADS_URL}${data?.image?.data?.attributes?.url}`}
           />
         </div>
-        <section
-          className="col-span-12"
+        <div
+          className={`${styles.content} col-span-8 col-start-3`}
           dangerouslySetInnerHTML={{ __html: content }}
-        ></section>
+        />
       </Container>
     </main>
   );
