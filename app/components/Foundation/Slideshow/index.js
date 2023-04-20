9be91e095228c4956dba0e3b08626527a879c8ad
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-04-20 11:31:43 -0300

    feat: slideshow finalizado

--- app/components/Foundation/Slideshow/index.js
+++ app/components/Foundation/Slideshow/index.js
@@ -1,73 +1,83 @@
 'use client';
 import Image from 'next/image';
-import { Autoplay, EffectFade, Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { useSwiper } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
+import { useState } from 'react';
 
 export default function Slideshow() {
-  const SwiperButtonPrev = ({ children }) => {
-    const swiper = useSwiper();
-    return (
-      <button onClick={() => swiper.slidePrev()} type="button">
-        {children}
-      </button>
-    );
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slides = [
+    {
+      url: 'slide-1.jpg'
+    },
+    {
+      url: 'slide-2.jpg'
+    }
+  ];
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
   };
 
-  const SwiperButtonNext = ({ children }) => {
-    const swiper = useSwiper();
-    return (
-      <button onClick={() => swiper.slideNext()} type="button">
-        {children}
-      </button>
-    );
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
   };
 
   return (
-    <>
-      <Swiper
-        autoplay={{ delay: 10000 }}
-        className="w-full"
-        effect={'fade'}
-        loop={true}
-        modules={[Autoplay, EffectFade, Navigation]}
-      >
-        <SwiperSlide>
-          <img alt="Sempre tecnologia" src="/slide-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img alt="Sempre tecnologia" src="/slide-2.jpg" />
-        </SwiperSlide>
-        <div className="absolute container flex justify-end left-0 mx-auto right-0 top-60 z-10">
-          <ul className="mr-6">
-            <li>
-              <SwiperButtonPrev>
-                <Image
-                  alt="Voltar"
-                  height={47}
-                  quality={100}
-                  src="/icon-arrow-left.svg"
-                  width={47}
-                />
-              </SwiperButtonPrev>
+    <div className="h-[480px] w-full m-auto relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full bg-center bg-cover duration-500"
+      ></div>
+      <div className="absolute container flex justify-end left-0 mx-auto right-0 top-[50%] translate-y-[-50%] z-10">
+        <ul className="mr-6">
+          <li className="cursor-pointer mb-2">
+            <Image
+              alt="Voltar"
+              height={47}
+              onClick={prevSlide}
+              quality={100}
+              src="/icon-arrow-left.svg"
+              width={47}
+            />
+          </li>
+          <li className="cursor-pointer">
+            <Image
+              alt="Voltar"
+              height={47}
+              onClick={nextSlide}
+              quality={100}
+              src="/icon-arrow-right.svg"
+              width={47}
+            />
+          </li>
+        </ul>
+      </div>
+      <div className="absolute container flex left-0 mx-auto right-0 bottom-[5%]">
+        <ul className="flex space-x-4">
+          {slides.map((slide, slideIndex) => (
+            <li
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className={`cursor-pointer p-2 ${
+                currentIndex === slideIndex && 'border-b border-b-white'
+              }`}
+            >
+              <span
+                className={`font-sans font-semibold text-white ${
+                  currentIndex !== slideIndex && 'text-opacity-60'
+                }`}
+              >{`0${slideIndex + 1}`}</span>
             </li>
-            <li>
-              <SwiperButtonNext>
-                <Image
-                  alt="Voltar"
-                  height={47}
-                  quality={100}
-                  src="/icon-arrow-right.svg"
-                  width={47}
-                />
-              </SwiperButtonNext>
-            </li>
-          </ul>
-        </div>
-      </Swiper>
-    </>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
