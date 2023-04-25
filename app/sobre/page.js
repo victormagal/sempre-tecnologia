0cdc63ced466eda93b9f248c0a8f29762e1c33a7
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-04-25 01:15:58 -0300

    retira estilo desnecessario

--- app/sobre/page.js
+++ app/sobre/page.js
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from './page.module.css';
 import { ContactForm, Tabs } from '../components/Elements';
 import { Container } from '../components/Foundation';
 import {
