e7aea7dc2c500ac70ecb825258a013610a736100
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-07-03 21:39:16 -0300

    feat: redireciona rota a1 para certificado a1

---
+++ app/a1/page.js
@@ -0,0 +1,12 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function Parceria() {
+  useEffect(() => {
+    window.location.replace(
+      'https://semprecertificadodigital.com.br/certificado-a1.html'
+    );
+  }, []);
+
+  return false;
+}
