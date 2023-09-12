d11819424737209d56ae499e030ee31557c5ade8
antoniojunio <juniordsj@gmail.com>
2023-09-12 13:35:59 -0300

    add tela de contato

--- app/contato/page.js
+++ app/contato/page.js
@@ -1,24 +1,149 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { neutralDark, neutralLight, neutralMid, red } from '../base/Colors';
+import { Overline, Text, Title } from '../base/Typography';
 import { ContactForm } from '../components/Elements';
-import { Container, HeroPage, ModalForm } from '../components/Foundation';
-import {
-  faEnvelope,
-  faPhone,
-  faArrowRight
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container } from '../components/Foundation';
 
 export default function Contato() {
-  const [openModal, setOpenModal] = useState(false);
-
   return (
     <main>
-      <ModalForm open={openModal} onClose={() => setOpenModal(false)} />
-      <HeroPage arrow={false} cta={false} mini={true} uri="/bg-blog.svg" />
-      <Container newClasses="py-24">
+      <Container bgColor={neutralLight[100]} newClasses="py-16">
+        <div className="col-span-12 lg:col-span-6 lg:col-start-4 flex flex-col md:space-y-6 text-center px-8 mt-32 mb-12">
+          <Overline appearance="o1" color={red[700]}>
+            FALE CONOSCO
+          </Overline>
+          <Title appearance="h2" color={neutralDark[500]} extra>
+            Nós adoraríamos ouvir de você
+          </Title>
+          <Text appearance="p1" color={neutralMid[500]}>
+            A nossa equipe está sempre à disposição para conversar com você!
+          </Text>
+        </div>
+      </Container>
+      <Container>
+        <div className="flex flex-row space-x-12 col-span-12 justify-center border-b-2 pb-24">
+          <Link
+            href="/sobre"
+            className="col-span-3 flex flex-col items-center space-y-4"
+          >
+            <Image
+              alt="Background Image"
+              className="cursor-pointer object-cover object-center"
+              height={20}
+              quality={100}
+              src="/icon-email.svg"
+              width={40}
+            />
+            <h1 className="font-serif font-semibold text-dark-blue text-xl">
+              Email
+            </h1>
+            <p className="font-sans text-soft-gray text-center text-sm">
+              Estamos aqui para atender você <br /> e tirar suas dúvidas.
+            </p>
+            <p className="font-sans font-bold text-red-600 text-sm">
+              contato@sempretecnologia.com.br
+            </p>
+          </Link>
+          <Link
+            className="col-span-3 flex flex-col items-center my-6 lg:my-0 space-y-4"
+            href="https://api.whatsapp.com/send?phone=556130839390"
+            target="_blank"
+          >
+            <Image
+              alt="Background Image"
+              className="object-cover object-center"
+              height={20}
+              quality={100}
+              src="/icon-atendimento.svg"
+              width={40}
+            />
+            <h1 className="font-serif font-semibold text-dark-blue text-xl">
+              Central de atendimetno
+            </h1>
+            <p className="font-sans text-soft-gray text-center text-sm">
+              De segunda a sexta (8h às 18h)
+            </p>
+            <p className="font-sans font-bold text-red-600 text-sm">
+              0800 941 6260
+            </p>
+          </Link>
+          <Link
+            className="col-span-3 flex flex-col items-center my-6 lg:my-0 space-y-4"
+            href="https://api.whatsapp.com/send?phone=556130839390"
+            target="_blank"
+          >
+            <Image
+              alt="Background Image"
+              className="object-cover object-center"
+              height={20}
+              quality={100}
+              src="/icon-whatsapp.svg"
+              width={40}
+            />
+            <h1 className="font-serif font-semibold text-dark-blue text-xl">
+              Whatsapp
+            </h1>
+            <p className="font-sans text-soft-gray text-center text-sm">
+              De segunda a sexta (8h às 18h)
+            </p>
+            <p className="font-sans font-bold text-red-600 text-sm">
+              Mandar mensagem
+            </p>
+          </Link>
+          <Link
+            className="col-span-3 flex flex-col items-center my-6 lg:my-0 space-y-4"
+            href="https://api.whatsapp.com/send?phone=556130839390"
+            target="_blank"
+          >
+            <Image
+              alt="Background Image"
+              className="object-cover object-center"
+              height={20}
+              quality={100}
+              src="/icon-contato.svg"
+              width={40}
+            />
+            <h1 className="font-serif font-semibold text-dark-blue text-xl">
+              Ligamos para você
+            </h1>
+            <p className="font-sans text-soft-gray text-center text-sm">
+              De segunda a sexta (8h às 18h)
+            </p>
+            <p className="font-sans font-bold text-red-600 text-sm">
+              Agendar ligacão
+            </p>
+          </Link>
+        </div>
+      </Container>
+      <Container>
+        <div className="col-span-12 lg:col-span-5 lg:col-start-7 mt-24 lg:order-last order-first">
+          <Image
+            className=""
+            alt="Portal Sempre Contador"
+            height={674}
+            width={466}
+            src="/img-contato.png"
+          />
+        </div>
+        <div className="lg:col-start-2 lg:col-span-5 col-span-12 mt-24">
+          <Title
+            appearance="h2"
+            className="mb-4"
+            color={neutralDark[600]}
+            extra
+          >
+            Ficou interessado?
+          </Title>
+          <Text appearance="p4" className="mb-8" color={neutralDark[100]}>
+            Temos vários benefícios exclusivos especialmente para a sua
+            necessidade. Venha ser um parceiro!
+          </Text>
+          <ContactForm />
+        </div>
+      </Container>
+      {/* <Container newClasses="py-24">
         <div className="col-span-4 lg:col-span-6 flex flex-col">
           <h2 className="font-serif font-semibold mb-2 text-gray-600 uppercase text-center lg:text-left">
             Fale conosco
@@ -66,101 +191,7 @@ export default function Contato() {
         <div className="col-span-4 lg:col-span-6">
           <ContactForm />
         </div>
-      </Container>
-      <section className="bg-overlay-gray py-24">
-        <Container>
-          <div className="col-span-4 lg:col-span-12 flex justify-center mb-8">
-            <h1 className="font-serif font-semibold text-dark-blue text-4xl text-center lg:text-left">
-              Um pouco mais sobre a Sempre
-            </h1>
-          </div>
-          <Link
-            href="/sobre"
-            className="col-span-4 flex flex-col items-center space-y-4"
-          >
-            <Image
-              alt="Background Image"
-              className="cursor-pointer object-cover object-center"
-              height={20}
-              quality={100}
-              src="/icon-quem-somos.svg"
-              width={50}
-            />
-            <h1 className="font-serif font-semibold text-dark-blue text-2xl">
-              Quem Somos
-            </h1>
-            <p className="font-sans text-soft-gray text-center">
-              Conheça a história da Sempre e venhar trabalhar conosco
-            </p>
-            <p className="font-sans font-bold text-red-600 text-sm">
-              Saiba mais
-              <span>
-                <FontAwesomeIcon
-                  className="h-3 ml-2 text-red-600"
-                  icon={faArrowRight}
-                />
-              </span>
-            </p>
-          </Link>
-          <Link
-            className="col-span-4 flex flex-col items-center my-6 lg:my-0 space-y-4"
-            href="https://api.whatsapp.com/send?phone=556130839390"
-            target="_blank"
-          >
-            <Image
-              alt="Background Image"
-              className="object-cover object-center"
-              height={20}
-              quality={100}
-              src="/icon-whatsapp-red.svg"
-              width={20}
-            />
-            <h1 className="font-serif font-semibold text-dark-blue text-2xl">
-              Chat WhatsApp
-            </h1>
-            <p className="font-sans text-soft-gray text-center">
-              Fale com nossos atendentes através do WhatsApp.
-            </p>
-            <p className="font-sans font-bold text-red-600 text-sm">
-              Entre em contato
-              <span>
-                <FontAwesomeIcon
-                  className="h-3 ml-2 text-red-600"
-                  icon={faArrowRight}
-                />
-              </span>
-            </p>
-          </Link>
-          <div
-            className="col-span-4 cursor-pointer flex flex-col items-center space-y-4"
-            onClick={() => setOpenModal(true)}
-          >
-            <Image
-              alt="Background Image"
-              className="object-cover object-center"
-              height={20}
-              quality={100}
-              src="/icon-agendar-horario.svg"
-              width={20}
-            />
-            <h1 className="font-serif font-semibold text-dark-blue text-2xl">
-              Agende um horário
-            </h1>
-            <p className="font-sans text-soft-gray text-center">
-              Agende uma ligação com nossos atendentes
-            </p>
-            <p className="font-sans font-bold text-red-600 text-sm">
-              Agendar ligação
-              <span>
-                <FontAwesomeIcon
-                  className="h-3 ml-2 text-red-600"
-                  icon={faArrowRight}
-                />
-              </span>
-            </p>
-          </div>
-        </Container>
-      </section>
+      </Container> */}
     </main>
   );
 }
