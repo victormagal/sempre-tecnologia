a4cbbb00000f477909e00ecce9a104305f8d76dc
antoniojunio <juniordsj@gmail.com>
2023-09-20 22:01:44 -0300

    Home desktop

--- app/components/Elements/Tabs/index.js
+++ app/components/Elements/Tabs/index.js
@@ -14,9 +14,9 @@ export default function Tabs() {
   return (
     <>
       <div className="lg:col-span-12">
-        <ul className="flex flex-row">
+        <ul className="flex flex-row gap-1 text-center">
           <li
-            className={`border-b-0 flex-auto w-8 ${
+            className={`border-b-0 flex-auto ${
               toggle === 1 ? 'bg-red-600' : 'bg-[#F9FAFB]'
             } p-3 cursor-pointer`}
             onClick={() => toggleTab(1)}
@@ -24,14 +24,14 @@ export default function Tabs() {
             <button
               className={`${
                 toggle === 1 ? 'text-white' : 'text-soft-gray'
-              } self-auto	font-sans font-normal text-lg text-left`}
+              } self-auto	font-sans font-normal text-lg`}
               type="button"
             >
               Distribuidores
             </button>
           </li>
           <li
-            className={`border-b-0 flex-auto w-8 w-auto ${
+            className={`border-b-0 flex-auto ${
               toggle === 2 ? 'bg-red-600' : 'bg-[#F9FAFB]'
             } p-3 cursor-pointer`}
             onClick={() => toggleTab(2)}
@@ -39,14 +39,14 @@ export default function Tabs() {
             <button
               className={`${
                 toggle === 2 ? 'text-white' : 'text-soft-gray'
-              } self-auto	font-sans font-normal text-lg text-left`}
+              } self-auto	font-sans font-normal text-lg text-center`}
               type="button"
             >
               Pequenos varejos
             </button>
           </li>
           <li
-            className={`border-b-0 flex-auto w-8 w-auto ${
+            className={`border-b-0 flex-auto ${
               toggle === 3 ? 'bg-red-600' : 'bg-[#F9FAFB]'
             } p-3 cursor-pointer`}
             onClick={() => toggleTab(3)}
@@ -54,14 +54,14 @@ export default function Tabs() {
             <button
               className={`${
                 toggle === 3 ? 'text-white' : 'text-soft-gray'
-              } self-auto	font-sans font-normal text-lg text-left`}
+              } self-auto	font-sans font-normal text-lg text-center`}
               type="button"
             >
               Prestadores de serviços
             </button>
           </li>
           <li
-            className={`border-b-0 flex-auto w-8 w-auto ${
+            className={`border-b-0 flex-auto ${
               toggle === 4 ? 'bg-red-600' : 'bg-[#F9FAFB]'
             } p-3 cursor-pointer`}
             onClick={() => toggleTab(4)}
@@ -69,14 +69,14 @@ export default function Tabs() {
             <button
               className={`${
                 toggle === 4 ? 'text-white' : 'text-soft-gray'
-              } self-auto	font-sans font-normal text-lg text-left`}
+              } self-auto	font-sans font-normal text-lg text-center`}
               type="button"
             >
               Recorrentes
             </button>
           </li>
           <li
-            className={`border-b-0 flex-auto w-8 w-auto ${
+            className={`border-b-0 flex-auto ${
               toggle === 5 ? 'bg-red-600' : 'bg-[#F9FAFB]'
             } p-3 cursor-pointer`}
             onClick={() => toggleTab(5)}
@@ -84,7 +84,7 @@ export default function Tabs() {
             <button
               className={`${
                 toggle === 5 ? 'text-white' : 'text-soft-gray'
-              } self-auto	font-sans font-normal text-lg text-left`}
+              } self-auto	font-sans font-normal text-lg text-center`}
               type="button"
             >
               Documentos fiscais
@@ -92,9 +92,9 @@ export default function Tabs() {
           </li>
         </ul>
       </div>
-      <div className=" col-span-12 ">
-        <div className={`${toggle === 1 ? 'block' : 'hidden'} flex flex-row `}>
-          <div className="basis-1/2 mr-12">
+      <div className="col-span-12">
+        <div className={`${toggle === 1 ? 'block' : 'hidden'} flex flex-row`}>
+          <div className="basis-1/2 py-24 ml-16">
             <Image
               alt="Sempre gestor"
               className="inline m-6 ml-0"
@@ -111,11 +111,74 @@ export default function Tabs() {
               src="/logos/tab-sempre-lite.png"
               width={93}
             />
-            <Title appearance="h2" color={neutralDark[500]} extra>
+            <Title
+              className="py-6"
+              appearance="h2"
+              color={neutralDark[500]}
+              extra
+            >
               Distribuidores
             </Title>
             <Text appearance="p1" color={neutralMid[500]}>
-              Pescados, bovinos e suínos Pães e salgados Hortifruti Comésticos
+              Pães e salgados
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Hortifruti
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Comésticos
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Picolés e sorvetes
+            </Text>
+          </div>
+          <div className="basis-1/2 ml-16">
+            <Image
+              alt="Sempre mensalidade"
+              className="mt-6"
+              height={410}
+              width={486}
+              quality={100}
+              src="/distriibuidores-tab.png"
+            />
+          </div>
+        </div>
+        <div className={`${toggle === 2 ? 'block' : 'hidden'} flex flex-row`}>
+          <div className="basis-1/2 py-24 ml-16">
+            <Image
+              alt="Sempre gestor"
+              className="inline m-6 ml-0"
+              height={16}
+              quality={100}
+              src="/logos/tab-sempre-gestor.png"
+              width={104}
+            />
+            <Image
+              alt="Sempre light"
+              className="inline m-6 ml-0"
+              height={16}
+              quality={100}
+              src="/logos/tab-sempre-lite.png"
+              width={93}
+            />
+            <Title
+              className="py-6"
+              appearance="h2"
+              color={neutralDark[500]}
+              extra
+            >
+              Pequenos varejos
+            </Title>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Pães e salgados
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Hortifruti
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Comésticos
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
               Picolés e sorvetes
             </Text>
           </div>
@@ -130,45 +193,8 @@ export default function Tabs() {
             />
           </div>
         </div>
-        <div className={`${toggle === 2 ? 'block' : 'hidden'} flex flex-row`}>
-          <div className="basis-1/2 mr-12">
-            <Image
-              alt="Sempre gestor"
-              className="inline m-6 ml-0"
-              height={16}
-              quality={100}
-              src="/logos/tab-sempre-gestor.png"
-              width={104}
-            />
-            <Image
-              alt="Sempre light"
-              className="inline m-6 ml-0"
-              height={16}
-              quality={100}
-              src="/logos/tab-sempre-lite.png"
-              width={93}
-            />
-            <Title appearance="h2" color={neutralDark[500]} extra>
-              Pequenos varejos
-            </Title>
-            <Text appearance="p1" color={neutralMid[500]}>
-              Essa solução é ideal para negócios que atendam majoritariamente
-              pessoa física e que não necessitem de um sistema específico.
-            </Text>
-          </div>
-          <div className="basis-1/2">
-            <Image
-              alt="Sempre mensalidade"
-              className="mt-6"
-              height={441}
-              quality={100}
-              src="/icon-service-02.svg"
-              width={435}
-            />
-          </div>
-        </div>
         <div className={`${toggle === 3 ? 'block' : 'hidden'} flex flex-row`}>
-          <div className="basis-1/2 mr-12">
+          <div className="basis-1/2 py-24 ml-16">
             <Image
               alt="Sempre gestor"
               className="inline m-6 ml-0"
@@ -185,28 +211,40 @@ export default function Tabs() {
               src="/logos/tab-sempre-lite.png"
               width={93}
             />
-            <Title appearance="h2" color={neutralDark[500]} extra>
+            <Title
+              className="py-6"
+              appearance="h2"
+              color={neutralDark[500]}
+              extra
+            >
               Prestadores de serviços
             </Title>
             <Text appearance="p1" color={neutralMid[500]}>
-              Esta solução é indicada para clientes que desejam focar seu tempo
-              encontrando estratégias comerciais, sem deixar de cumprir a
-              obrigatoriedade da emissão de nota fiscal
+              Pães e salgados
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Hortifruti
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Comésticos
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Picolés e sorvetes
             </Text>
           </div>
           <div className="basis-1/2">
             <Image
               alt="Sempre mensalidade"
               className="mt-6"
-              height={441}
+              height={410}
+              width={486}
               quality={100}
-              src="/icon-service-04.svg"
-              width={435}
+              src="/distriibuidores-tab.png"
             />
           </div>
         </div>
         <div className={`${toggle === 4 ? 'block' : 'hidden'} flex flex-row`}>
-          <div className="basis-1/2 mr-12">
+          <div className="basis-1/2 py-24 ml-16">
             <Image
               alt="Sempre gestor"
               className="inline m-6 ml-0"
@@ -223,28 +261,40 @@ export default function Tabs() {
               src="/logos/tab-sempre-lite.png"
               width={93}
             />
-            <Title appearance="h2" color={neutralDark[500]} extra>
+            <Title
+              className="py-6"
+              appearance="h2"
+              color={neutralDark[500]}
+              extra
+            >
               Recorrentes
             </Title>
             <Text appearance="p1" color={neutralMid[500]}>
-              O Sempre Mensalidade é um sistema de gestão e controle de
-              recorrência. Essa solução é ideal para empresas que cobram
-              mensalidade dos seus clientes.
+              Pães e salgados
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Hortifruti
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Comésticos
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Picolés e sorvetes
             </Text>
           </div>
           <div className="basis-1/2">
             <Image
               alt="Sempre mensalidade"
               className="mt-6"
-              height={441}
+              height={410}
+              width={486}
               quality={100}
-              src="/icon-service-05.svg"
-              width={435}
+              src="/distriibuidores-tab.png"
             />
           </div>
         </div>
         <div className={`${toggle === 5 ? 'block' : 'hidden'} flex flex-row`}>
-          <div className="basis-1/2 mr-12">
+          <div className="basis-1/2 py-24 ml-16">
             <Image
               alt="Sempre gestor"
               className="inline m-6 ml-0"
@@ -261,23 +311,35 @@ export default function Tabs() {
               src="/logos/tab-sempre-lite.png"
               width={93}
             />
-            <Title appearance="h2" color={neutralDark[500]} extra>
+            <Title
+              className="py-6"
+              appearance="h2"
+              color={neutralDark[500]}
+              extra
+            >
               Documentos fiscais
             </Title>
             <Text appearance="p1" color={neutralMid[500]}>
-              Esta solução é indicada para clientes que desejam focar seu tempo
-              encontrando estratégias comerciais, sem deixar de cumprir a
-              obrigatoriedade da emissão de nota fiscal
+              Pães e salgados
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Hortifruti
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Comésticos
+            </Text>
+            <Text appearance="p1" color={neutralMid[500]}>
+              Picolés e sorvetes
             </Text>
           </div>
           <div className="basis-1/2">
             <Image
               alt="Sempre mensalidade"
               className="mt-6"
-              height={441}
+              height={410}
+              width={486}
               quality={100}
-              src="/icon-service-07.svg"
-              width={435}
+              src="/distriibuidores-tab.png"
             />
           </div>
         </div>
