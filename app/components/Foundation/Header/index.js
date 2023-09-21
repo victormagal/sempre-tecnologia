9486377287fef7598790337578e8ca4b5a8e2f40
antoniojunio <juniordsj@gmail.com>
2023-09-21 17:04:48 -0300

    add estrutura header navigation

--- app/components/Foundation/Header/index.js
+++ app/components/Foundation/Header/index.js
@@ -84,7 +84,265 @@ export default function Header() {
                   e.stopPropagation();
                 }}
               >
-                <li>Olá enfermeira</li>
+                <span>Distribuidores</span>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/pescados.svg"
+                    width={27}
+                  />
+                  Pescados, bovinos e suínos
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/paes.svg"
+                    width={27}
+                  />
+                  Pães e salgados
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/hortifruti.svg"
+                    width={27}
+                  />
+                  Hortifruti
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/cosmeticos.svg"
+                    width={27}
+                  />
+                  Comésticos
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/picoles.svg"
+                    width={27}
+                  />
+                  Picolés e sorvetes
+                </li>
+
+                <span>Pequenos varejos</span>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/vestuarios.svg"
+                    width={27}
+                  />
+                  Vestuários e calçados
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/barbearias.svg"
+                    width={27}
+                  />
+                  Barbearias e salões de beleza
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/petshops.svg"
+                    width={27}
+                  />
+                  Petshops
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/lanchonetes.svg"
+                    width={27}
+                  />
+                  Lanchonetes e quiosques
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/moveis.svg"
+                    width={27}
+                  />
+                  Móveis e colchões
+                </li>
+
+                <span>Prestadores de serviço</span>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/contabilidades.svg"
+                    width={27}
+                  />
+                  Contabilidades e BPO
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/construtoras.svg"
+                    width={27}
+                  />
+                  Construtoras e Engenharias
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/consultorios.svg"
+                    width={27}
+                  />
+                  Consultórios
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/seguranca.svg"
+                    width={27}
+                  />
+                  Segurança, limpeza e conservação
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/manutencao.svg"
+                    width={27}
+                  />
+                  Manutenção e instalações
+                </li>
+
+                <span>Recorrentes</span>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/associacoes.svg"
+                    width={27}
+                  />
+                  Associações e sindicatos
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/clubes.svg"
+                    width={27}
+                  />
+                  Clubes
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/cursos.svg"
+                    width={27}
+                  />
+                  Cursos
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/condominios.svg"
+                    width={27}
+                  />
+                  Condomínios
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/locacoes.svg"
+                    width={27}
+                  />
+                  Locações e serviços
+                </li>
+
+                <span>Documentos fiscais</span>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/nfprodutos.svg"
+                    width={27}
+                  />
+                  NF-e Nota Fiscal Produtos
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/nfservicos.svg"
+                    width={27}
+                  />
+                  NF-e Nota Fiscal Serviços
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/conhecimento.svg"
+                    width={27}
+                  />
+                  CT-e Conhecimento de transporte
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/manifesto.svg"
+                    width={27}
+                  />
+                  MDF-e Manifesto destinatário
+                </li>
+                <li>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/nfconsumidor.svg"
+                    width={27}
+                  />
+                  NFC-e Nota Fiscal Consumidor
+                </li>
               </ul>
             </li>
             <li>
@@ -107,7 +365,32 @@ export default function Header() {
                   e.stopPropagation();
                 }}
               >
-                <li>Olá enfermeira</li>
+                <span>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/parceiro.svg"
+                    width={27}
+                  />
+                  Programa de parceria para Contadores
+                </span>
+                <li>Conheça e aproveite benefícios exclusivos.</li>
+
+                <span>
+                  <Image
+                    alt="Sempre mensalidade - Sistemas web para gestão de recorrência"
+                    height={24}
+                    quality={100}
+                    src="/menu-icons/parceiro.svg"
+                    width={27}
+                  />
+                  Seja um parceiro certificador
+                </span>
+                <li>
+                  Programa de parceria para venda de Certificado Digital e
+                  Sistemas Web de Gestão.
+                </li>
               </ul>
             </li>
             <li>
