16e1e5b0283e098c0fe407a23e631fbd56f188c7
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-10-03 17:36:11 -0300

    feat(new-version): pagina sobre refatorada

--- app/sobre/page.js
+++ app/sobre/page.js
@@ -1,6 +1,5 @@
 'use client';
 import Image from 'next/image';
-import styles from './page.module.css';
 import { neutralDark, neutralLight, neutralMid, red } from '../base/Colors';
 import { Overline, Text, Title } from '../base/Typography';
 import { CardFeature } from '../components/Elements';
@@ -8,17 +7,17 @@ import { Container } from '../components/Foundation';
 
 export default function Sobre() {
   return (
-    <main className="pt-36">
-      <Container newClasses="mb-16">
-        <div className="col-span-4">
+    <main className="pt-24">
+      <Container newClasses="my-16">
+        <div className="col-span-4 flex flex-col space-y-4">
           <Overline appearance="o1" color={red[700]}>
             Quem somos
           </Overline>
-          <Title appearance="h2" color={neutralDark[500]} extra={true}>
+          <Title appearance="h2" color={neutralDark[500]} extra>
             Oi, somos a Sempre Tecnologia.
           </Title>
         </div>
-        <div className="col-end-13 col-span-6">
+        <div className="col-span-4 lg:col-span-6 lg:col-end-13 flex items-center">
           <Text appearance="p3" color={neutralMid[600]}>
             Nós somos especializados em desenvolver sistemas de gestão
             empresarial com tecnologia própria e atendimento super capacitado
@@ -28,67 +27,64 @@ export default function Sobre() {
           </Text>
         </div>
       </Container>
-      <section className={styles.azul}>
-        <Container>
-          <div
-            className="col-span-4 lg:col-span-12 flex justify-center relative"
-            style={{ height: '539px' }}
-          >
-            <Image
-              alt="Sempre mensalidade"
-              fill
-              sizes="100vw"
-              src="/about.png"
-              style={{ objectFit: 'contain' }}
-            />
+      <Container bgColor="linear-gradient(180deg, transparent 0, transparent 85px, #091E42 0, #091E42 100%);">
+        <div className="col-span-4 lg:col-span-12 flex h-[153px] sm:h-[200px] md:h-[300px] lg:h-[350px] xl:h-[539px] justify-center relative">
+          <Image
+            alt="Sempre mensalidade"
+            fill
+            sizes="100vw"
+            src="/about.png"
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+      </Container>
+      <Container bgColor={neutralDark[500]}>
+        <div className="col-span-4 lg:col-span-8 lg:col-start-3 flex flex-col lg:flex-row lg:justify-between p-16 space-y-8 lg:space-y-0">
+          <div className="flex flex-col justify-center items-center">
+            <Title appearance="h1" color="white">
+              +25
+            </Title>
+            <Title appearance="h6" color="#B3BAC5">
+              filiais no Brasil
+            </Title>
           </div>
-          <div className="col-span-8 col-start-3 flex justify-between mb-16 mt-12">
-            <div className="flex flex-col justify-center items-center">
-              <Title appearance="h1" color="white">
-                +25
-              </Title>
-              <Title appearance="h6" color="#B3BAC5">
-                filiais no Brasil
-              </Title>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <Title appearance="h1" color="white">
-                +10
-              </Title>
-              <Title appearance="h6" color="#B3BAC5">
-                anos no mercado
-              </Title>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <Title appearance="h1" color="white">
-                99%
-              </Title>
-              <Title appearance="h6" color="#B3BAC5">
-                de satisfação no atendimento
-              </Title>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <Title appearance="h1" color="white">
-                99%
-              </Title>
-              <Title appearance="h6" color="#B3BAC5">
-                de satisfação no pós-venda
-              </Title>
-            </div>
+          <div className="flex flex-col justify-center items-center">
+            <Title appearance="h1" color="white">
+              +10
+            </Title>
+            <Title appearance="h6" color="#B3BAC5">
+              anos no mercado
+            </Title>
           </div>
-        </Container>
-      </section>
-      <Container>
-        <div className="col-span-6 col-start-4 my-16 text-center">
+          <div className="flex flex-col justify-center items-center">
+            <Title appearance="h1" color="white">
+              99%
+            </Title>
+            <Title appearance="h6" color="#B3BAC5">
+              de satisfação no atendimento
+            </Title>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <Title appearance="h1" color="white">
+              99%
+            </Title>
+            <Title appearance="h6" color="#B3BAC5">
+              de satisfação no pós-venda
+            </Title>
+          </div>
+        </div>
+      </Container>
+      <Container newClasses="py-16">
+        <div className="col-span-4 lg:col-span-6 lg:col-start-4 flex flex-col items-center space-y-6">
           <Title
             appearance="h2"
-            className="mb-4"
+            className="text-center"
             color={neutralDark[500]}
             extra
           >
             Temos a solução perfeita para a sua empresa.
           </Title>
-          <Text appearance="p1" color={neutralMid[500]}>
+          <Text appearance="p1" className="text-center" color={neutralMid[500]}>
             Tecnologia própria e atendimento diferenciado perto de você.
           </Text>
         </div>
@@ -99,6 +95,7 @@ export default function Sobre() {
           description="Sempre juntos para apoiar as empresas e gerar prosperidade."
           icon="faBuilding"
           iconColor={red[600]}
+          iconSize="h-10"
           title="Propósito"
         />
         <CardFeature
@@ -106,6 +103,7 @@ export default function Sobre() {
           description="Facilitar a vida dos empresários e contadores com soluções em tecnologia e atendimento diferenciado."
           icon="faCircleDot"
           iconColor={red[600]}
+          iconSize="h-10"
           title="Missão"
         />
         <CardFeature
@@ -113,6 +111,7 @@ export default function Sobre() {
           description="Superação, simplicidade, espírito de equipe, melhoria contínua, profissionalismo, respeito e entusiasmo."
           icon="faGem"
           iconColor={red[600]}
+          iconSize="h-10"
           title="Valores"
         />
         <CardFeature
@@ -120,6 +119,7 @@ export default function Sobre() {
           description="Ser referência em sistemas de gestão e certificado digital no Brasil de forma sustentável."
           icon="faLightbulb"
           iconColor={red[600]}
+          iconSize="h-10"
           title="Visão"
         />
       </Container>
