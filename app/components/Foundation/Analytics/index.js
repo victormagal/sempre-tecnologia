574a5fca6d30ca2fbb918ad5bfe411041771de72
Victor Arruda Magalhaes <victor.arruda.magalhaes@gmail.com>
2023-10-30 14:29:13 -0300

    feat: aplica ga4 tag

--- app/components/Foundation/Analytics/index.js
+++ app/components/Foundation/Analytics/index.js
@@ -4,6 +4,11 @@ import Script from 'next/script';
 export default function Analytics() {
   return (
     <>
+      <Script src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/0fd1df83-5a56-43bf-997a-bb5f444a714e-loader.js" />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-HGCL2DK72C"
+      />
       <noscript>
         <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-N8Z66M2"
@@ -17,10 +22,20 @@ export default function Analytics() {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N8Z66M2');`
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N8Z66M2');`
+        }}
+      />
+      <Script
+        id="ga4-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HGCL2DK72C');`
         }}
       />
       <Script
@@ -31,10 +46,9 @@ export default function Analytics() {
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "58acgh3ffd");`
+            })(window, document, "clarity", "script", "58acgh3ffd");`
         }}
       />
-      <Script src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/0fd1df83-5a56-43bf-997a-bb5f444a714e-loader.js" />
     </>
   );
 }
